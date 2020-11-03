6cd39ba6e43fcab664e674f46be1b771591af92a
Jenniffer <jenlpac@yahoo.com>
2020-11-03 10:52:53 -0600

    add questions for each readme file section

--- index.js
+++ index.js
@@ -8,39 +8,90 @@ const generateMarkdown = require('./utils/generateMarkdown');
 const questions = [
     {
         // Add Title:
-
+        type: "input",
+        name: "title",
+        message: "What is the title of your application? (Required)",
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log("Please enter the title of your project.");
+                return false;
+            }
+        }
     },
     {
         // Add Description:
-
-    },
-    {
-        // Add Table of Contents:
-
+        type: "input",
+        name: "description",
+        message: "Please provide a description of your application (include your motivation for building it). (Required)",
+        validate: descriptInput => {
+            if (descriptInput) {
+                return true;
+            } else {
+                console.log("Please enter a description of your project.");
+                return false;
+            }
+        }
     },
     {
         // Add Installation instructions:
-
+        type: "input",
+        name: "installation",
+        message: "Please provide instructions on how to install your application. Include dependencies that are required.",
     },
     {
-        // Add Usage section with screenshots:
-
+        // Add Usage section:
+        type: "input",
+        name: "usage",
+        message: "Please provide instructions and examples of how to use your application."
     },
     {
         // Add License section (append badge to top):
-
+        type: "list",
+        name: "license",
+        message: "What license is your application covered under?",
+        choices: [
+            'MIT',
+            'Apache 2.0',
+            'MPL 2.0',
+            'GPL 2.0',
+            'None'
+        ]
     },
     {
         // Add Contributing guidelines:
+        type: "input",
+        name: "contributing",
+        message: "Provide guidelines on how someone can participate in development."
 
     },
     {
         // Add Test(s) instructions:
+        type: "input",
+        name: "tests",
+        message: "Provide instructions for testing application.",
+        default: "npm test"
 
     },
+    // Add Questions/Contact section:
     {
-        // Add Questions/Contact section:
-
+        type: "input",
+        name: "github",
+        message: "What is your Github user name? (Required)",
+        valideate: githubInput => {
+            if (githubInput) {
+                return true;
+            } else {
+                console.log("Please enter your github user name.");
+                return false;
+            }
+        }
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your email address?"
     }
 
 ];
