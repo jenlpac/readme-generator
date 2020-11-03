9b1b73b81fde08fab95a2509dba4752d5fa8438b
Jenniffer <jenlpac@yahoo.com>
2020-11-03 10:06:29 -0600

    complete questions template

--- index.js
+++ index.js
@@ -1,7 +1,47 @@
-
+// Add File System and Inquirer for use:
+const fs = require('fs');
+const inquirer = require('inquirer');
+// Add generateMarkdown file for use:
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // array of questions for user
 const questions = [
+    {
+        // Add Title:
+
+    },
+    {
+        // Add Description:
+
+    },
+    {
+        // Add Table of Contents:
+
+    },
+    {
+        // Add Installation instructions:
+
+    },
+    {
+        // Add Usage section with screenshots:
+
+    },
+    {
+        // Add License section (append badge to top):
+
+    },
+    {
+        // Add Contributing guidelines:
+
+    },
+    {
+        // Add Test(s) instructions:
+
+    },
+    {
+        // Add Questions/Contact section:
+
+    }
 
 ];
 
